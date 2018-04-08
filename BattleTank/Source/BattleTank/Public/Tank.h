edc1e12c9afbb1e1f98be275c2c3ef8ca8d7c7a8
fennecfoxes <sk9260@gmail.com>
2018-04-07 17:09:59 -0700

    Added health bar on top of tanks

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -15,6 +15,10 @@ public:
 	// Called by engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
+	// Returns current health as percentage of starting health between 0 and 1
+	UFUNCTION(BlueprintPure, Category = Health)
+	float GetHealthPercent() const;
+
 private:
 	// Sets default values for this pawn's properties
 	ATank();
